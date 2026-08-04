6dcaab43c9dff94b4d9d6632ae5470863f64a6b1
Sandeep <singhsandeepoff@gmail.com>
2026-08-04 14:59:37 +0530

    End-to-End project

---
+++ src/test/java/Login.java
@@ -0,0 +1,13 @@
+//import org.openqa.selenium.By;
+//import org.testng.annotations.Test;
+//
+//public class Login extends RegisterUser{
+//
+//    @Test
+//    public void login()
+//    {
+//        findElement(By.id("userEmail")).sendKeys(dynamicEmail);
+//        findElement(By.id("userPassword")).sendKeys(fixedPassword);
+//        findElement(By.id("login")).click();
+//    }
+//}
