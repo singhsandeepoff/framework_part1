16a4e119ab300438e39648616bf433bb1d50b2b6
Sandeep <singhsandeepoff@gmail.com>
2026-08-13 19:35:15 +0530

    Fix cucumber tags and json reporting

--- src/test/java/framework/cucumber/TestNGTestRunner.java
+++ src/test/java/framework/cucumber/TestNGTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features="src/test/java/framework/cucumber", glue="framework.stepDefinition", monochrome = true, tags = "@Regression", plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"})
+@CucumberOptions(features="src/test/java/framework/cucumber", glue="framework.stepDefinition", monochrome = true, plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"})
 
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 
