77e441c0fcd88a3c9285881ff22a706af34798f9
Sandeep <singhsandeepoff@gmail.com>
2026-08-13 17:34:02 +0530

    Restored Cucumber setup on local staging branch

---
+++ src/test/java/framework/cucumber/TestNGTestRunner.java
@@ -0,0 +1,10 @@
+package framework.cucumber;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features="src/test/java/framework/cucumber", glue="framework.stepDefinition", monochrome = true,plugin = {"html:target/cucumber.html"})
+
+public class TestNGTestRunner extends AbstractTestNGCucumberTests {
+
+}
---
+++ src/test/java/framework/stepDefinition/MyStepdefs.java
@@ -0,0 +1,74 @@
+package framework.stepDefinition;
+
+import framework.pageobjects.*;
+import framework.testcomponents.BaseTest;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.ITestResult;
+
+import java.io.IOException;
+import java.util.List;
+
+public abstract class MyStepdefs {
+
+    BaseTest baseTest = new BaseTest() {
+        @Override
+        public void onFinish(ITestResult result) {
+
+        }
+    };
+
+    public LandingPage landingPage;
+    public ProductCatalogue productCatalogue;
+    public ResultPage resultPage;
+    String confirmMessage;
+
+
+    @Before
+    public void setupScenario() throws IOException {
+        // 🟢 Cleanly launch app before every Cucumber scenario
+        landingPage = baseTest.launchApplication();
+    }
+    @Given("I landed on Ecommerce Page")
+    public void iLandedOnEcommercePage(){
+    }
+
+    @Given("^Logged in with username (.+) and password (.+)$")
+    public void loggedInWithUsernameNameAndPasswordPassword(String username, String password) throws Throwable {
+        productCatalogue = landingPage.loginApp(username, password);
+    }
+
+    @When("^I add product (.+) to Cart$")
+    public void iAddProductProductNameToCart(String productName) throws Throwable {
+        List<WebElement> products = productCatalogue.getProductList();
+        productCatalogue.addProductToCart(productName);
+    }
+
+    @And("^Checkout (.+) and submit the order$")
+    public void checkoutProductNameAndSubmitTheOrder(String productName) throws Throwable {
+        CartPage cartPage = productCatalogue.goToCartPage();
+        Boolean match = cartPage.VerifyProdDisplay(productName);
+        Assert.assertTrue(match, "Product was not found in cart!");
+        CheckoutPage checkoutPage = cartPage.goToCheckout();
+        checkoutPage.selectCountry("India");
+        resultPage = checkoutPage.submitOrder();
+    }
+
+    @Then("{string} message is displayed on ConfirmationPage")
+    public void messageIsDisplayedOnConfirmationPage(String string) {
+        confirmMessage = resultPage.getConfirmationMessage();
+        Assert.assertTrue(confirmMessage.equalsIgnoreCase(string));
+    }
+
+    @After
+    public void tearDownScenario()
+    {
+        baseTest.tearDown();
+    }
+}
