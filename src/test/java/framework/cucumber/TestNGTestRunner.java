d8e4cc95eff55b4cc1f2bc1583eab52a4b42c672
Sandeep <singhsandeepoff@gmail.com>
2026-08-13 19:05:47 +0530

    cucumber reports on jenkins

--- src/test/java/framework/cucumber/TestNGTestRunner.java
+++ src/test/java/framework/cucumber/TestNGTestRunner.java
@@ -4,10 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features="src/test/java/framework/cucumber", glue="framework.stepDefinition", monochrome = true, tags = "@Regression", plugin = {"html:target/cucumber.html"})
+@CucumberOptions(features="src/test/java/framework/cucumber", glue="framework.stepDefinition", monochrome = true, tags = "@Regression", plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"})
 
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
-
-
 
 }
